3323d148038676617bcd691a9867fe3e3abbf1db
moparisthebest <admin@moparisthebest.com>
2016-11-05 21:37:43 -0400

    Only delete/resign sig if it changes

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ cd aur
 cp -a /etc/pacman.conf /etc/pacman.d/mirrorlist ./
 
 # sign everything
-gpg -u 'ECB9B8CBAAC68C03!' -b pacman.conf
-gpg -u 'ECB9B8CBAAC68C03!' -b mirrorlist
+gpg -v pacman.conf.sig 2>/dev/null || (rm -f pacman.conf.sig; gpg -u 'ECB9B8CBAAC68C03!' -b pacman.conf)
+gpg -v mirrorlist.sig 2>/dev/null || (rm -f mirrorlist.sig; gpg -u 'ECB9B8CBAAC68C03!' -b mirrorlist)
 
 rsync -av --stats --progress --delete ./ root@mytorrentflux:/pacman/aur/
